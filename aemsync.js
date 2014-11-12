3c0e4d4e516b4aad2ae027ca6afd1ecc70de10a8
Michal Kochel <gavoja@gmail.com>
2014-11-12 20:28:34 +0100

    Replaced buggy adm-zip with archiver.

--- aemsync.js
+++ aemsync.js
@@ -4,15 +4,15 @@
 
 	// Built-in packages
 	var os = require("os");
-	var fs = require("fs");
 	var path = require('path');
 	var parseUrl = require('url').parse;
 	var StringDecoder = require('string_decoder').StringDecoder;
 
 	// NPM packages
+	var fs = require('graceful-fs');
 	var watch = require("node-watch");
 	var minimist = require('minimist');
-	var AdmZip = require("adm-zip");
+	var archiver = require('archiver'); // TODO: consider using zip-stream for less dependencies.
 	var FormData = require('form-data');
 
 	// Constants
@@ -23,35 +23,73 @@
 	var RE_CONTENT = /.*\.content\.xml$/;
 	// Include files on "jcr_root/xyz/..." path that's outside hidden or target folder.
 	var RE_SAFE_PATH = /^((?!(\/\.)|(\/target\/)).)*\/jcr_root\/[^\/]*\/.*$/;
+	var PACKAGE_PATH = DEBUG ? __dirname + "/aemsync.zip" : os.tmpdir() + "/aemsync.zip";
 
-	var syncerInterval = 500;
+	// Variables.
+	var syncerInterval = 100;
 	var queue = [];
+	var debugMode = false;
 
 	/** Prints debug message. */
 	function debug(msg) {
-		if (DEBUG) {
+		if (debugMode) {
 			console.log(msg);
 		}
 	}
 
-	/** Prints package info. */
-	function debugPack(pack) {
-		if (!DEBUG) {
-			return;
-		}
-
-		console.log("\nPackage contents:");
-		pack.zip.getEntries().forEach(function(zipEntry) {
-			console.log("  " + zipEntry.entryName);
+	/** Recursively walks over directory. */
+	function walkSync(dir, includeDirectories) {
+		var results = includeDirectories ? [dir] : [];
+		var list = fs.readdirSync(dir);
+		list.forEach(function(file) {
+			file = dir + "/" + file;
+			var stat = fs.statSync(file);
+			if (stat && stat.isDirectory()) {
+				results = results.concat(walkSync(file));
+			} else {
+				results.push(file);
+			}
 		});
-		console.log("Package filters:");
-		console.log(pack.filters);
-		console.log("");
+		return results;
 	}
 
+	/** Gets a zip path from a local path. */
+	function getZipPath(localPath) {
+		return localPath.replace(/.*\/(jcr_root\/.*)/, "$1");
+	}
+
+	/** Gets a filter path from a local path. */
+	function getFilterPath(localPath) {
+		return localPath.replace(/(.*jcr_root)|(\.xml$)|(\.dir)/g, "").replace(/\/_([^\/]*)_([^\/]*)$/g, "$1:$2");
+	}
+
+	/** Zip wrapper. */
+	function Zip() {
+		var zip = archiver("zip");
+		var output = fs.createWriteStream(PACKAGE_PATH);
+		zip.pipe(output);
+
+		this.addLocalFile = function(localPath, zipPath) {
+			debug("  Zipping: " + zipPath);
+			zip.append(fs.createReadStream(localPath), {name: zipPath});
+		};
+
+		this.addFile = function(content, zipPath) {
+			debug("  Zipping: " + zipPath);
+			zip.append(content, {name: zipPath});
+		};
+
+		this.save = function(callback) {
+			output.on("close", callback);
+			zip.finalize();
+		};
+	}
+
+	/** Pushes changes to AEM. */
 	function Syncer(targets, queue) {
 		targets = targets.split(",");
 
+		/** Submits the package manager form. */
 		var sendForm = function(zipPath) {
 			for (var i=0; i<targets.length; ++i) {
 				var params = parseUrl(targets[i]);
@@ -69,6 +107,7 @@
 			}
 		};
 
+		/** Package install submit callback */
 		var formSubmitCallback = function(err, res) {
 			if (!res) {
 				console.log(this._headers.host + " " + err.code);
@@ -85,50 +124,33 @@
 			});
 		};
 
-		var getZipPath = function(filePath) {
-			return filePath.replace(/.*\/(jcr_root\/.*)/, "$1");
-		};
-
-		var getFilterPath = function(filePath) {
-			return filePath.replace(/(.*jcr_root)|(\.xml$)|(\.dir)/g, "").replace(/_([^\/]*)_([^\/]*)$/g, "$1:$2");
-		};
-
+		/** Creates a package. */
 		var createPackage = function() {
-			var zip = new AdmZip();
-			zip.addLocalFolder(__dirname + "/data/package_content");
-			return {zip: zip, filters: "" };
+			var zip = new Zip();
+			var path = __dirname + "/data/package_content";
+			var fileList = walkSync(path, false);
+			fileList.forEach(function(subItem) {
+				zip.addLocalFile(subItem, subItem.substr(path.length + 1));
+			});
+			return {zip: zip, filters: ""};
 		};
 
+		/** Installs a package. */
 		var installPackage = function(pack) {
 			// Add filters.
 			// TODO: Add support for rep:policy nodes.
 			pack.filters = '<?xml version="1.0" encoding="UTF-8"?>\n<workspaceFilter version="1.0">\nFILTERS</workspaceFilter>'.replace(/FILTERS/g, pack.filters);
-			pack.zip.addFile("META-INF/vault/filter.xml", new Buffer(pack.filters));
+			pack.zip.addFile(new Buffer(pack.filters), "META-INF/vault/filter.xml");
 
-			debugPack(pack);
+			debug("\nPackage filters:\n" + pack.filters + "\n");
 
-			// TODO: Make in-memory zip perhaps?
-			var zipPath = os.tmpdir() + "/aemsync.zip";
-			pack.zip.writeZip(zipPath);
-			sendForm(zipPath);
-		};
-
-		/** Recursively walks over directory */
-		var walk = function(dir) {
-			var results = [dir];
-			var list = fs.readdirSync(dir);
-			list.forEach(function(file) {
-				file = dir + "/" + file;
-				var stat = fs.statSync(file);
-				if (stat && stat.isDirectory()) {
-					results = results.concat(walk(file));
-				} else {
-					results.push(file);
-				}
+			pack.zip.save(function() {
+				// TODO: Make in-memory zip perhaps?
+				sendForm(PACKAGE_PATH);
 			});
-			return results;
 		};
 
+		/** Adds item to package. */
 		var addItemInPackage = function(pack, item) {
 
 			console.log("ADD: " + item);
@@ -143,41 +165,34 @@
 
 			// Add file.
 			if (fs.lstatSync(item).isFile()) {
-				pack.zip.addLocalFile(item, path.dirname(getZipPath(item)));
+				pack.zip.addLocalFile(item, getZipPath(item));
 				return;
 			}
 
 			// Add files in directory.
-			var fileList = walk(item);
+			var fileList = walkSync(item, true);
 			fileList.forEach(function(subItem) {
 
 				// Add files
 				if (fs.lstatSync(subItem).isFile()) {
-					pack.zip.addLocalFile(subItem, path.dirname(getZipPath(subItem)));
+					pack.zip.addLocalFile(subItem, getZipPath(subItem));
 					return;
 				}
 
 				// Add NT_FOLDER if no .content.xml.
-				if (!fs.existsSync(subItem + "/.content.xml")) {
-					pack.zip.addLocalFile(NT_FOLDER, getZipPath(subItem));
+				var contentXml = subItem + "/.content.xml";
+				if (!fs.existsSync(contentXml)) {
+					pack.zip.addLocalFile(NT_FOLDER, getZipPath(contentXml));
 				}
 			});
 		};
 
+		/** Deletes item in package. */
 		var deleteItemInPackage = function(pack, item) {
 			console.log("DEL: " + item);
 
 			var filterPath = getFilterPath(item);
 			pack.filters += '  <filter root="FILE" />\n'.replace(/FILE/g, filterPath);
-		};
-
-		/** Processes local path. */
-		var processItem = function(pack, item) {
-			if (fs.existsSync(item)) {
-				addItemInPackage(pack, item);
-			} else {
-				deleteItemInPackage(pack, item);
-			}
 		};
 
 		/** Processes queue items; duplicates and descendants are removed. */
@@ -233,7 +248,11 @@
 
 			var pack = createPackage();
 			for (item in dict) {
-				processItem(pack, item);
+				if (fs.existsSync(item)) {
+					addItemInPackage(pack, item);
+				} else {
+					deleteItemInPackage(pack, item);
+				}
 			}
 			installPackage(pack);
 		};
@@ -241,6 +260,7 @@
 		setInterval(this.processQueue, syncerInterval);
 	}
 
+	/** Watches for file system changes. */
 	function Watcher(pathToWatch, queue) {
 		pathToWatch = path.resolve(path.normalize(pathToWatch));
 		if (!fs.existsSync(pathToWatch)) {
@@ -263,6 +283,8 @@
 			return;
 		}
 		syncerInterval = args.i || syncerInterval;
+		debugMode = args.d;
+
 		new Watcher(args._[0], queue);
 		new Syncer(args.t, queue);
 	}
